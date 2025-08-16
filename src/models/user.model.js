69c0aed69005a883acd6daa2189ee5cda44b91ce
Piyush Thakur <piyushthakur9183@gmail.com>
2025-08-16 20:59:15 +0530

    First Commit

---
+++ src/models/user.model.js
@@ -0,0 +1,54 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    profilePic: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/dicxtgova/image/upload/v1755354896/profile_wkq9wi.png",
+    },
+    resetPasswordToken: {
+      type: String,
+      default: null,
+    },
+    resetPasswordExpires: {
+      type: Date,
+      default: null,
+    },
+    groups:{
+      type:[mongoose.Schema.Types.ObjectId],
+      default:[],
+      ref:"Group",
+    },
+    privacy:{
+      type:Boolean,
+      default:false
+    },
+    username:{
+      type:String,
+      maxLength:18,
+      unique:true,
+      required:true,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
